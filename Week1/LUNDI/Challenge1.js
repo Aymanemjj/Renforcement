1ca3f88456655defcba99a3413389c654f439f6c
Aymane <aymanemj@proton.me>
2026-03-18 14:44:36 +0000

    first challenger

---
+++ Week1/LUNDI/Challenge1.js
@@ -0,0 +1,13 @@
+let table = ['ahmed', 'salah', 'aymane', 'hamza', "tayed", 'mohamed'];
+console.log(table)
+console.log(table[0])
+console.log(table[table.length-1])
+console.log(table.length)
+
+table.push('zakaria')
+table.push('Jhon Doe')
+
+let last = table.pop()
+console.log(last)
+table.shift()
+console.log(table)
